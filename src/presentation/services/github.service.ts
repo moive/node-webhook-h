4c14e7a972778485ffe0e8be780b225512b5aa59
Moises Velasquez <moises007@gmail.com>
2026-02-09 01:17:56 -0500

    chore(message): return message of github

--- src/presentation/services/github.service.ts
+++ src/presentation/services/github.service.ts
@@ -1,4 +1,4 @@
-import { GithubStarPayload } from "../../interfaces";
+import { GithubIssuesPayload, GithubStarPayload } from "../../interfaces";
 
 export class GithubService {
   constructor() {}
@@ -10,5 +10,20 @@ export class GithubService {
     message = `User ${sender.login} ${action} star on ${repository.full_name}`;
 
     return message;
+  }
+
+  onIssue(payload: GithubIssuesPayload): string {
+    const { action, issue } = payload;
+
+    if (action === "opened") {
+      return `An issue was opened with this title ${issue.title}`;
+    }
+    if (action === "closed") {
+      return `An issue was closed with this title ${issue.user.login}`;
+    }
+    if (action === "reopened") {
+      return `An issue was reopened with this title ${issue.user.login}`;
+    }
+    return `Unknown action ${action}`;
   }
 }
