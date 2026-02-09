c88846544cbc0ba3b45d0d76ad3f2fbf742a8a9e
Moises Velasquez <moises007@gmail.com>
2026-02-09 01:38:12 -0500

    feat(discord): add discord service

--- src/presentation/github/github.controller.ts
+++ src/presentation/github/github.controller.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from "express";
 import { GithubService } from "../services/github.service";
+import { DiscordService } from "../services/discord.service";
 
 export class GithubController {
-  constructor(private readonly githubService = new GithubService()) {}
+  constructor(
+    private readonly githubService = new GithubService(),
+    private readonly discordService = new DiscordService(),
+  ) {}
 
   webhookHandler = (req: Request, res: Response) => {
     const githubEvent = req.header("x-github-event") ?? "unknown";
@@ -26,7 +30,9 @@ export class GithubController {
         message = `Unknown event ${githubEvent}`;
         break;
     }
-    console.log(message);
-    res.status(202).send("Accepted!!");
+    this.discordService
+      .nofity(message)
+      .then(() => res.status(202).send("Accepted!!"))
+      .catch(() => res.status(500).send("Internal Server Error"));
   };
 }
---
+++ src/presentation/services/discord.service.ts
@@ -0,0 +1,28 @@
+import { envs } from "../../config";
+
+export class DiscordService {
+  private readonly discordWebhookUrl = envs.DISCORD_WEBHOOK_URL;
+
+  constructor() {}
+
+  async nofity(message: string) {
+    const body = {
+      content: message,
+    };
+
+    const resp = await fetch(this.discordWebhookUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!resp.ok) {
+      console.log("Error sending message to discord");
+      return false;
+    }
+
+    return true;
+  }
+}
